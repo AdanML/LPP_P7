b9dfba18489b63a24558fceabe2dc46e60cc4ed5
AdanML <adanmendezlemus@gmail.com>
2012-11-01 23:10:45 +0000

    comparable

--- spec/rspec_Fraccion.rb
+++ spec/rspec_Fraccion.rb
@@ -4,6 +4,7 @@ describe Fraccion do
    before :each do
       @obj1 = Fraccion.new(1,2)
       @obj2 = Fraccion.new(2,4)
+      @obj3 = Fraccion.new(3,4)
 
    end
    
@@ -30,6 +31,27 @@ describe Fraccion do
    it "Se debe mostrar por la consola con decimal 0.5" do
       @obj1.to_f.should == 0.5
    end
+
+   it "Se deben comparar si dos fracciones so iguales con ==" do
+      (@obj1==@obj2).should == true
+   end
+
+   it "Se debe comparar si una fraccion es menor que otra con <" do
+      (@obj1<@obj3).should == true
+   end
+
+   it "Se debe comparar si una fraccion es mayor que otra con >" do
+      (@obj3>@obj1).should == true
+   end
+
+   it "Se debe comparar si una fraccion es menor o igual que otra con <=" do
+      (@obj1<=@obj2).should == true and (@obj1<=@obj3).should == true
+   end
+
+   it "Se debe comparar si una fraccion es mayor o igual que otra con >=" do
+      (@obj2>=@obj1).should == true and (@obj3>=@obj1).should == true
+   end
+  
 
 end
 
